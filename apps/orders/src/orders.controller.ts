bc8cb005ad6d1dd1d0214902a27e301e4e5214ee
bdwxxx <bogbldan@gmail.com>
2025-04-14 13:25:46 +0000

    remove trash

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -4,9 +4,5 @@ import { OrdersService } from './orders.service';
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
-
-  @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
-  }
+  
 }
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -2,7 +2,5 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+
 }
